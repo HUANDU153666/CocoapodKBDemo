e99471ca6ebcd48e4bac94152e832ba711366fbc
HUANDU153666 <HUAN153666@163.com>
2020-08-11 11:05:03 +0800

    test

---
+++ Example/Pods/Target Support Files/CocoapodKBDemo/CocoapodKBDemo-umbrella.h
@@ -0,0 +1,28 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CTMediator+FTDCalendar.h"
+#import "FTDCalendarController.h"
+#import "FTDCalendarModel.h"
+#import "FTDCalendarRequestManager.h"
+#import "FTDCalendarViewModel.h"
+#import "GFCalendar.h"
+#import "GFCalendarCell.h"
+#import "GFCalendarMonth.h"
+#import "GFCalendarScrollView.h"
+#import "GFCalendarView.h"
+#import "NSDate+GFCalendar.h"
+#import "Target_FTDCalendar.h"
+
+FOUNDATION_EXPORT double CocoapodKBDemoVersionNumber;
+FOUNDATION_EXPORT const unsigned char CocoapodKBDemoVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-CocoapodKBDemo_Tests/Pods-CocoapodKBDemo_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CocoapodKBDemo_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CocoapodKBDemo_Tests
+@end
